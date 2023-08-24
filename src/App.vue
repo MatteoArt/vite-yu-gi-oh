01ec09ee8a7bc30c680f34962fe6e9e87f653602
MatteoArt <mattrim8@gmail.com>
2023-08-24 17:08:07 +0200

    header applicazione

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <script>
 
+import TheHeader from "./components/TheHeader.vue";
+
+export default {
+  components: {
+    TheHeader,
+  }
+}
+
 </script>
 
 <template>
-  
+     <TheHeader></TheHeader>
+
+     <main>
+      
+     </main>
 </template>
 
 <style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,51 @@
+<script>
+
+export default {
+    data () {
+        return {
+            titleObj: {
+                img: "yugioh.png",
+                title: "Yu-Gi-Oh Api"
+            }
+        }
+    },
+    methods: {
+        getImg(nomeImg) {
+            return new URL(`../assets/${nomeImg}`, import.meta.url).href;
+        }
+    }
+}
+
+</script>
+
+<template>
+    <header>
+        <div class="logo-img">
+            <img :src="getImg(titleObj.img)" alt="logo">
+        </div>
+        <h1> {{ titleObj.title }} </h1>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+
+header {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 30px;
+    padding: 10px 15px;
+
+    h1 {
+        font-size: 38px;
+    }
+
+    .logo-img {
+        width: 90px;
+
+        img {
+            width: 100%;
+        }
+    }
+}
+</style>
